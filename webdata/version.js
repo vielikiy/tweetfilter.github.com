f55b380a002a3892a2d2f4625f9370f0d4817261
Tweetfilter <blacksmilingface@gmail.com>
2011-07-20 22:01:07 +0200

    updated version

--- webdata/version.js
+++ webdata/version.js
@@ -1,9 +1,9 @@
 var version = [
  {  
    'number': '2.0',
-   'updated': '2011-07-18',
-   'minified-size': 136311,
-   'uncompressed-size': 187206,
-   'debug-size': 211492
+   'updated': '2011-07-20',
+   'minified-size': 136373,
+   'uncompressed-size': 187268,
+   'debug-size': 211568
  }  
 ];
