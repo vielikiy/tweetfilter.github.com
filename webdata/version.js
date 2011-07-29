1e32046fe770c57cfa2c01be53e2070934823c35
Tweetfilter <blacksmilingface@gmail.com>
2011-07-30 00:23:10 +0200

    updated to 2.0.3

--- webdata/version.js
+++ webdata/version.js
@@ -1,7 +1,7 @@
 var version = [
  {  
-   'number': '2.0.2',
-   'updated': '2011-07-29',
+   'number': '2.0.3',
+   'updated': '2011-07-30',
    'minified-size': 129628,
    'uncompressed-size': 184811,
    'debug-size': 210527
