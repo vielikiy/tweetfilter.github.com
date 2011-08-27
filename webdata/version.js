bcb5c5a8709416ca490e0ca005637217ea62bf93
Tweetfilter <blacksmilingface@gmail.com>
2011-08-27 23:19:57 +0200

    added thank you page

--- webdata/version.js
+++ webdata/version.js
@@ -4,6 +4,7 @@ var version = [
    'updated': '2011-08-25',
    'minified-size': 174579,
    'uncompressed-size': 242994,
-   'debug-size': 290618
+   'debug-size': 290618,
+   'thankyou': '<div id="thankyou"><img src="webdata/coffeesmile.jpg" /><h1>Thanks a lot for your donation.</h1><h2>May your cup be ever full!</h2></div>'
  }  
 ];
