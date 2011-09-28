9b2e095e48c12b4b8d4013749483861aef495195
Tweetfilter <blacksmilingface@gmail.com>
2011-09-28 23:22:14 +0200

    updated to version 2.0.7

--- webdata/version.js
+++ webdata/version.js
@@ -1,10 +1,10 @@
 var version = [
  {  
-   'number': '2.0.6',
-   'updated': '2011-09-04',
-   'minified-size': 184260,
-   'uncompressed-size': 307086,
-   'debug-size': 290618,
+   'number': '2.0.7',
+   'updated': '2011-09-28',
+   'minified-size': 184031,
+   'uncompressed-size': 256231,
+   'debug-size': 307742,
    'thankyou': '<div id="thankyou"><img src="webdata/coffeesmile.jpg" /><h1>Thanks a lot for your donation.</h1><h2>May your cup be ever full!</h2></div>'
  }  
 ];
